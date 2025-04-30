1dc53ab632c7d90f853134cea4097865903e45fc
Rami <2151703@cegepmontpetit.ca>
2025-04-30 17:20:01 -0400

    Déploiement en dev : update image te namespace

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Danny!\n');
+    res.end('Image docker dev sur environement Dev\n');
 });
 
 
