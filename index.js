f4dc58f4ce5203b17ac820161390063da23ca9ed
Rami <2151703@cegepmontpetit.ca>
2025-04-30 17:28:34 -0400

    aaaaa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Image docker dev sur environement awdwfajilhilWFE;HILFDev\n');
+    res.end('Image docker dev surddddd environement awdwfajilhilWFE;HILFDev\n');
 });
 
 
